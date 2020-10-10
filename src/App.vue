0992c1b62957e7c898810c18437a4d9d5bcc9269
Jacques Ramphal <jacques.r@myplanet.com>
2020-10-10 14:02:54 -0400

    styles

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome Jacques"/>
-    <HelloWorld1 msg="Welcome Jacques"/>
+    <MainNav/>
+    <WorkPlay/>
+    <MainFooter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import HelloWorld1 from './components/HelloWorld.vue'
+import MainNav from './components/MainNav.vue'
+import WorkPlay from './components/WorkPlay.vue'
+import MainFooter from './components/MainFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    HelloWorld1
+    MainNav,
+    WorkPlay,
+    MainFooter
   }
 }
 </script>
 
 <!-- global styles -->
-<style>
-#app {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>  
+  @import './assets/styles/all.css';
+  
 </style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <header class="">
+      <h3 class="subtle showmobile">Jake Ramphal</h3>
+      <h3 class="subtle hidemobile">Jacques Ramphal</h3>
+      <h3 class="justify-end"><a href="#footer">Info</a></h3>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+     margin: 0 0 2.8em 0;
+     display: grid;
+     grid-gap: 0;
+     grid-template-columns: repeat(2, 1fr);
+}
+</style>
