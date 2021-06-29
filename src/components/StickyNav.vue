ee51ee91e2d16a11f218285fa81afe15a01249b8
Jacques Ramphal <jacques.r@myplanet.com>
2021-06-28 20:55:41 -0400

    nav

--- src/components/StickyNav.vue
+++ src/components/StickyNav.vue
@@ -1,82 +1,79 @@
 <template>
-  <div
-    class="container navbar"
-    :class="{ 'hidden-navbar': !showNavbar }"
-  >
-  <div class="bg"
-    data-aos="slide-up"
-    data-aos-duration="1000"
-    data-aos-delay="0"
-    data-aos-once="true"
-    data-aos-mirror="true"
-    data-aos-anchor-placement="top"
+  <div class="container navbar" :class="{ 'hidden-navbar': !showNavbar }">
+    <div
+      class="bg"
+      data-aos="slide-left"
+      data-aos-duration="1000"
+      data-aos-delay="0"
+      data-aos-once="true"
+      data-aos-mirror="true"
+      data-aos-anchor-placement="top"
+    >
+      <nav class="">
+        <ul class="justify-start">
+          <li
+            class=" nav-link"
+            data-aos-anchor-placement="top"
+            data-aos-delay="0"
+            data-aos-duration="1000"
+            data-aos-once="true"
+            data-aos="fade-left"
+            tabindex="2"
+          >
+            <router-link to="Info">Info</router-link>
+          </li>
+          <li
+            class="nav-link"
+            data-aos="fade-left"
+            data-aos-duration="1000"
+            data-aos-delay="250"
+            data-aos-once="true"
+            data-aos-anchor-placement="top"
+            tabindex="3"
+          >
+            <router-link to="Work">Work</router-link>
+            <!--<router-link class="isDisabled" to="/">Work</router-link> -->
 
-  >
-    <nav class="">
-      <h1
-        class="hidemobile nav-link"
-        data-aos-anchor-placement="top"
-        data-aos-delay="500"
-        data-aos-duration="1000"
-        data-aos-once="true"
-        data-aos="fade-right"
-        id="wordmark"
-        tabindex="1"
-      >
-        <a href="#/">Jacques Ramphal</a>
-      </h1>
-
-      <h1
-        class="showmobile nav-link"
-        data-aos-anchor-placement="top"
-        data-aos-delay="500"
-        data-aos-duration="1000"
-        data-aos-once="true"
-        data-aos="fade-right"
-        id="wordmark"
-        tabindex="1"
-      >
-        <a href="#/">Jacques R.</a>
-      </h1>
-      <ul class="justify-end">
-        <li
-          class=" nav-link"
-          data-aos-anchor-placement="top"
-          data-aos-delay="1200"
-          data-aos-duration="1000"
-          data-aos-once="true"
-          data-aos="fade-left"
-          tabindex="2"
-        >
-          <router-link to="Info">Info</router-link>
-        </li>
-        <li
-          class="nav-link"
-          data-aos="fade-left"
-          data-aos-duration="1000"
-          data-aos-delay="1500"
-          data-aos-once="true"
-          data-aos-anchor-placement="top"
-          tabindex="3"
-        >
-           <router-link to="Work">Work</router-link>
-          <!--<router-link class="isDisabled" to="/">Work</router-link> -->
-
-          <!-- <router-link :to="{ 'info/'+'#info'}">Info</router-link> -->
-          <!-- <a href="mailto:jacques@ramphal.design">Email</a>
+            <!-- <router-link :to="{ 'info/'+'#info'}">Info</router-link> -->
+            <!-- <a href="mailto:jacques@ramphal.design">Email</a>
            <router-link to="Info">Info</router-link> -->
-        </li>
-      </ul>
-    </nav>
+          </li>
+        </ul>
+        <h1
+          class="hidemobile nav-link justify-end"
+          data-aos-anchor-placement="top"
+          data-aos-delay="500"
+          data-aos-duration="750"
+          data-aos-once="true"
+          data-aos="fade-left"
+          id="wordmark"
+          tabindex="1"
+        >
+          <a href="#/">Jacques Ramphal</a>
+        </h1>
+
+        <h1
+          class="showmobile nav-link justify-end"
+          data-aos-anchor-placement="top"
+          data-aos-delay="500"
+          data-aos-duration="750"
+          data-aos-once="true"
+          data-aos="fade-left"
+          id="wordmark"
+          tabindex="1"
+        >
+          <a href="#/">Jacques R.</a>
+        </h1>
+      </nav>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 /**
  * @component
  */
-const OFFSET = 60
+const OFFSET = 60;
 export default {
   name: "StickyNav",
   props: {
@@ -85,92 +82,95 @@ export default {
       default: "Jacques Ramphal",
     },
   },
-  data () {
+  data() {
     return {
       showNavbar: true,
       lastScrollPosition: 0,
-      scrollValue: 0
-    }
+      scrollValue: 0,
+    };
   },
 
-  mounted () {
-    this.lastScrollPosition = window.pageYOffset
-    window.addEventListener('scroll', this.onScroll)
-    const viewportMeta = document.createElement('meta')
-    viewportMeta.name = 'viewport'
-    viewportMeta.content = 'width=device-width, initial-scale=1'
-    document.head.appendChild(viewportMeta)
+  mounted() {
+    this.lastScrollPosition = window.pageYOffset;
+    window.addEventListener("scroll", this.onScroll);
+    const viewportMeta = document.createElement("meta");
+    viewportMeta.name = "viewport";
+    viewportMeta.content = "width=device-width, initial-scale=1";
+    document.head.appendChild(viewportMeta);
   },
 
-  beforeDestroy () {
-    window.removeEventListener('scroll', this.onScroll)
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
   },
 
   methods: {
-    onScroll () {
+    onScroll() {
       if (window.pageYOffset < 0) {
-        return
+        return;
       }
       if (Math.abs(window.pageYOffset - this.lastScrollPosition) < OFFSET) {
-        return
+        return;
       }
-      this.showNavbar = window.pageYOffset < this.lastScrollPosition
-      this.lastScrollPosition = window.pageYOffset
-    }
-  }
+      this.showNavbar = window.pageYOffset < this.lastScrollPosition;
+      this.lastScrollPosition = window.pageYOffset;
+    },
+  },
 };
 </script>
 <style scoped>
 * {
   color: inherit !important;
   mix-blend-mode: normal;
+  margin: 0;
 }
 
-.container, .navbar {
+.container,
+.navbar {
   padding: 0 !important;
   overflow: visible;
   mix-blend-mode: normal !important;
-  bottom: 1rem;
-  left: 1rem;
-  right: 1rem;
+  bottom: 1.6rem;
+  left: 1.6rem;
+  right: 1.6rem;
   position: fixed;
   z-index: 1000 !important;
   transform: translate3d(0, 0, 0) !important;
   transition: 0.4s all ease-in-out !important;
   width: auto;
-
 }
 .bg {
-    overflow: hidden;
+  overflow: hidden;
   border-radius: 4px;
   background: white;
-  padding: 1rem !important;
-  border: 1px solid;
-  border-color: var(--color-xxxlight);
-  box-shadow: var(--shadow-z1); 
   align-items: middle;
   justify-self: stretch;
-
+  padding: 2rem 1rem 2rem 1rem !important;
+  box-shadow: var(--shadow-z2);
 }
 .navbar.hidden-navbar {
   transform: translate3d(0, 150%, 0) !important;
 }
-
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
 nav {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   position: relative;
+  justify-self: stretch;
 }
-.nav-link{
+.nav-link > a {
   background: none;
-  padding: 1rem 1rem 0.5rem 1rem !important;
+  border-radius: 4px;
+  padding: 1rem !important;
+  text-decoration: none !important;
 }
-.nav-link:hover{
-
-    background: var(--color-xxxlight);
+.nav-link > a:hover {
+  background: rgba(0, 0, 0, 0.05);
 }
 #wordmark {
-  width: auto;
 }
 h1 {
   font-size: 2em;
@@ -182,37 +182,34 @@ li {
   line-height: 1;
   float: left;
   text-decoration: none;
-  padding-left: 2.8rem;
   list-style-type: none;
+  margin: 0;
 }
 li:first-child {
-  margin-right: 1rem;
+  padding-right: 1rem;
 }
 @media (prefers-color-scheme: dark) {
   .bg {
     background: var(--color-offblack);
-    border: 1px solid; 
+    border: 1px solid;
     border-color: black;
-
+  }
+  .nav-link > a:hover {
+    background: rgba(0, 0, 0, 0.25);
   }
 }
 /* ------------ BREAKPOINT MD ------------ */
 @media only screen and (min-width: 740px) {
-
-.container, .navbar {
-    bottom: 2rem;
-  left: 2rem;
-  right: 2rem;
-    transition: .8s all ease-in-out !important;
-
-}
-.bg {
+  .container,
+  .navbar {
+    transition: 0.8s all ease-in-out !important;
+  }
+  .bg {
     justify-self: end;
+  }
 
+  .navbar.hidden-navbar {
+    transform: translate3d(100%, 0, 0) !important;
+  }
 }
-.navbar.hidden-navbar {
-  transform: translate3d(100%, 0, 0) !important;
-}
-}
-
 </style>
