14e09c83e875ef742d81bbf287d44632ff695c95
Jacques Ramphal <jacques.r@myplanet.com>
2022-06-02 13:48:19 -0500

    remove vue config tests

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,5 @@ module.exports = {
 			.use("vue-svg-inline-loader")
 				.loader("vue-svg-inline-loader")
 				.options({});
-	},
-	devServer: {
-		port: 8080,
-		public: 'https://localhost:5001'
-	  },
+	}
 };
