c6e8fb12ad3fc6904454d0990d17484b8637eb6f
bob <jacques.r@myplanet.com>
2022-08-15 14:31:29 -0400

    storybook tests failed

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,10 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-  ],
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
   features: {
-    postcss: false,
+    postcss: false
   },
-}
+  core: {
+    builder: "webpack5"
+  }
+};
