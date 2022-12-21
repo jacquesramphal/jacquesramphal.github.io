5f9bf1be865f6e4aabe62cdcb1a37aa625c13b67
bob <jacques.r@myplanet.com>
2022-12-20 20:49:33 -0500

    test gsap animation working

--- src/pages/ProjectPage.vue
+++ src/pages/ProjectPage.vue
@@ -1,5 +1,6 @@
 <template>
   <PageWrapper id="project" class="">
+    <!-- Replace this transition component with lifecycle hooks, see vue 3 tutorial -->
     <transition appear @before-enter="beforeEnter" @enter="enter">
       <HeroBanner
         center
@@ -10,6 +11,7 @@
         :subtitle="`${entry.description}`"
         :tag="`${entry.tag}`"
     /></transition>
+
     <GridContainer class="animate glow delay-2">
       <ThumbLarge
         title=""
@@ -20,41 +22,42 @@
     </GridContainer>
 
     <!-- Section 1 - START -->
+    <!-- Replace this transition component with lifecycle hooks, see vue 3 tutorial -->
+    <transition appear @before-enter="beforeEnter" @enter="enter">
+      <GridWrapper id="section1">
+        <AnimatedComponent>
+          <GridContainer class="width">
+            <GridWrapper>
+              <TextBlock
+                left
+                eyebrow=""
+                header="Section 1"
+                :details="`${entry.description}`"
+                :blockquote="`${entry.description}`"
+              />
 
-    <GridWrapper id="section1">
-      <AnimatedComponent>
-        <GridContainer class="width">
-          <GridWrapper>
-            <TextBlock
-              left
-              eyebrow=""
-              header="Section 1"
-              :details="`${entry.description}`"
-              :blockquote="`${entry.description}`"
+              <TextBlock
+                left
+                eyebrow=""
+                header=""
+                :details="`${entry.description}`"
+              />
+            </GridWrapper>
+          </GridContainer>
+        </AnimatedComponent>
+        <AnimatedComponent>
+          <GridContainer id="" style="padding-top: 0 !important">
+            <ThumbLarge
+              class="width2"
+              title=""
+              filename="work/glo.svg"
+              alt="Jacques working at Myplanet"
+              caption="This is caption text."
             />
-
-            <TextBlock
-              left
-              eyebrow=""
-              header=""
-              :details="`${entry.description}`"
-            />
-          </GridWrapper>
-        </GridContainer>
-      </AnimatedComponent>
-      <AnimatedComponent>
-        <GridContainer id="" style="padding-top: 0 !important">
-          <ThumbLarge
-            class="width2"
-            title=""
-            filename="work/glo.svg"
-            alt="Jacques working at Myplanet"
-            caption="This is caption text."
-          />
-        </GridContainer>
-      </AnimatedComponent>
-    </GridWrapper>
-
+          </GridContainer>
+        </AnimatedComponent>
+      </GridWrapper>
+    </transition>
     <!-- Section 1 - END - Make this a mapped component ^ duplicate for # of sections in data-->
   </PageWrapper>
 </template>
@@ -62,6 +65,7 @@
 <script>
 import projectData from "@/assets/data/projects.json";
 
+// Import GSAP and ScrollTrigger
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
@@ -84,6 +88,7 @@ export default {
       el.style.transform = "translateY(-100px)";
     },
     // where the animation will end up
+    // NOTE: Find gsap set approach for animating components on load instead of all at oncuechange, see cv3 code and common problems thread
     enter(el) {
       gsap.to(el, {
         duration: 1,
