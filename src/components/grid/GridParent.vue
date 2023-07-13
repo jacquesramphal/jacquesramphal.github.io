56f50ef2dc4d83b7854535d828c4fdcbc56be3a4
Jake Ramphal <jacques.r@myplanet.com>
2023-07-13 09:15:35 -0400

    gridparent styling

--- src/components/grid/GridParent.vue
+++ src/components/grid/GridParent.vue
@@ -7,34 +7,27 @@
 <script>
 export default {
   name: "GridParent",
-  props: {
-    // tight: {
-    //   type: Boolean,
-    //   default: false,
-    // },
-    // fullvw: {
-    //   type: Boolean,
-    //   default: false,
-    // },
-    // fullvh: {
-    //   type: Boolean,
-    //   default: false,
-    // },
-  },
-  // computed: {
-  //   classes() {
-  //     return {
-  //       "container-spacing": true,
-  //       "container-spacing--tight": this.tight,
-  //       "container-spacing--normal": !this.tight,
-  //       "container-spacing--fullvw": this.fullvw,
-  //       "container-spacing--fullvh": this.fullvh,
-  //     };
-  //   },
-  // },
+  props: {},
 };
 </script>
 
 <style lang="scss" scoped>
-
+.grid-parent {
+  display: grid;
+  grid-gap: var(--spacing-sm);
+  grid-template-columns: repeat(1, 1fr);
+  grid-template-rows: repeat(1, 1fr);
+}
+@media only screen and (min-width: 740px) {
+  .grid-parent {
+    grid-template-columns: repeat(2, 1fr);
+    margin-top: 0;
+    grid-gap: var(--spacing-md);
+  }
+}
+@media only screen and (min-width: 1201px) {
+  .grid-parent {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
 </style>
