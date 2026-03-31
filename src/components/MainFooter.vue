d8b25d9b0213af1d8fe16647045812840142ded6
Jake Ramphal <jacquesramphal@users.noreply.github.com>
2026-03-30 20:30:03 -0400

    small tweaks to footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -383,14 +383,14 @@ $spacing-lg: var(--spacing-lg);
 $spacing-sm: var(--spacing-sm);
 
 #wrapper {
-  // border-block-start: var(--border);
+  border-block-start: var(--border);
   //  background: var(--background-darker);
-  // padding-block-end: $spacing-lg;
   @media only screen and (max-width: 768px) {
     padding-block-end: var(--spacing-lg);
   }
   @media only screen and (min-width: 1201px) {
     padding-block-end: inherit;
+    padding-block-start: var(--spacing-md);
   }
 }
 
